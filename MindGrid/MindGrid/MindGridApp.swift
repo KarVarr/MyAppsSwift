a86c0260c9ec2378a1e0d9c09c08e9d6b2a12dbf
KarVarr <megatr9n@gmail.com>
2023-04-13 19:18:07 +0300

    first commit

---
+++ MindGrid/MindGrid/MindGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindGridApp.swift
+//  MindGrid
+//
+//  Created by Karen Vardanian on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MindGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
