9e701d05be6a275690d9dbc7c6475ee640f9dd58
KarVarr <megatr9n@gmail.com>
2023-05-09 13:26:48 +0300

    added Chat with AI

---
+++ Chat with AI/Chat with AI/Chat_with_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_with_AIApp.swift
+//  Chat with AI
+//
+//  Created by Karen Vardanian on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chat_with_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
