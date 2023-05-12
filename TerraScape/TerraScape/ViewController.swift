64fb8dd04210f90b41eeee4e5cadfcf08ddb4f55
KarVarr <megatr9n@gmail.com>
2023-05-12 18:03:57 +0300

    added new project TerraScape

---
+++ TerraScape/TerraScape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TerraScape
+//
+//  Created by Karen Vardanian on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
