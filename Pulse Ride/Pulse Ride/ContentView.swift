cdc551f988e877245c35f79034bd48e1550f1c73
KarVarr <megatr9n@gmail.com>
2023-06-02 20:22:48 +0300

    Pulse Ride added new nowNowHaptic

--- Pulse Ride/Pulse Ride/ContentView.swift
+++ Pulse Ride/Pulse Ride/ContentView.swift
@@ -11,6 +11,8 @@ import CoreHaptics
 struct ContentView: View {
     @State private var engine: CHHapticEngine?
     @State private var isPlaying = false
+    @State private var count = 10
+    @State private var value = 0.5
     
     var body: some View {
         VStack {
@@ -19,7 +21,7 @@ struct ContentView: View {
                     .edgesIgnoringSafeArea(.all)
                 
                 StartScreenView()
-                  
+                
             }
             
             Text("Tap")
@@ -28,14 +30,15 @@ struct ContentView: View {
                     prepareHaptics()
                 }
                 .onTapGesture {
-                    if !isPlaying {
-                        startLoop()
-                    } else {
-                        stopLoop()
-                    }
-
+//                    triggerSelectionFeedback()
+//                    count = 10
+                     nomNomPattern()
                 }
-        
+            
+            Slider(value: $value, in: 0.1...1.0)
+                .padding()
+            
+            
         }
         
         
@@ -44,12 +47,22 @@ struct ContentView: View {
     
     func simpleSuccess() {
         let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
+        generator.notificationOccurred(.warning)
+    }
+    
+    func selectionFeedback() {
+        let feedbackGenerator = UISelectionFeedbackGenerator()
+        feedbackGenerator.selectionChanged()
+    }
+    
+    func impactFeedback() {
+        let impactGenerator = UIImpactFeedbackGenerator(style: .heavy)
+        impactGenerator.impactOccurred()
     }
     
     func prepareHaptics() {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-
+        
         do {
             engine = try CHHapticEngine()
             try engine?.start()
@@ -57,19 +70,26 @@ struct ContentView: View {
             print("There was an error creating the engine: \(error.localizedDescription)")
         }
     }
-
+    
     func complexSuccess() {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         var events = [CHHapticEvent]()
-
-        for val in stride(from: 0.5, to: 1, by: 0.1) {
+        
+        for val in stride(from: 0.1, to: 1, by: 0.1) {
             let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(val))
             let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(val))
             let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: val)
             events.append(event)
         }
         
-
+        for val in stride(from: 0.1, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - val))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - val))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + val)
+            events.append(event)
+        }
+        
+        
         do {
             let pattern = try CHHapticPattern(events: events, parameters: [])
             let player = try engine?.makePlayer(with: pattern)
@@ -79,19 +99,59 @@ struct ContentView: View {
         }
     }
     
-    func startLoop() {
-        isPlaying = true
-        DispatchQueue.global(qos: .background).async {
-            while self.isPlaying {
-                self.complexSuccess()
-                Thread.sleep(forTimeInterval: 1)
+    func triggerSelectionFeedback() {
+        guard count > 0 else {
+            return
+        }
+        
+        impactFeedback()
+        simpleSuccess()
+        selectionFeedback()
+
+        count -= 1
+        
+        if count > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                triggerSelectionFeedback()
             }
         }
     }
     
-    func stopLoop() {
-        isPlaying = false
+    private func nomNomPattern()  {
+        var events = [CHHapticEvent]()
+        
+      let rumble1 = CHHapticEvent(
+        eventType: .hapticContinuous,
+        parameters: [
+            CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(value)),
+          CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.8)
+        ],
+        relativeTime: 0,
+        duration: 1.5)
+        events.append(rumble1)
+      
+      let rumble2 = CHHapticEvent(
+        eventType: .hapticContinuous,
+        parameters: [
+          CHHapticEventParameter(parameterID: .hapticIntensity, value: 0.4),
+          CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.3)
+        ],
+        relativeTime: 1.5,
+        duration: 0.5)
+        events.append(rumble2)
+
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
+
+    
+  
     
 }
 
