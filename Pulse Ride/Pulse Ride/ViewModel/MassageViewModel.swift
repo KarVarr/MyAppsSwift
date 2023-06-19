7f5ab13dfb08bf84a5e17405c442059af5543dbf
KarVarr <megatr9n@gmail.com>
2023-06-19 16:45:21 +0300

    Pulse Ride return startVibration for one single vibration

--- Pulse Ride/Pulse Ride/ViewModel/MassageViewModel.swift
+++ Pulse Ride/Pulse Ride/ViewModel/MassageViewModel.swift
@@ -44,46 +44,31 @@ class MassageViewModel: ObservableObject {
         }
     }
     
-    private func startVibration() {
-        guard let engine = engine else { return }
-        
-        do {
-            try engine.start()
-            
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(valueOfIntensity))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.2)
-            let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: .infinity)
-            
-            let pattern = try CHHapticPattern(events: [event], parameters: [])
-            let player = try engine.makePlayer(with: pattern)
-            try player.start(atTime: 0)
-            
-            DispatchQueue.main.async {
-                self.isVibrating = true
-            }
-            
-            DispatchQueue.global(qos: .userInteractive).async {
-                while self.isVibrating {
-                    // Create a dynamic parameter for the intensity
-                    let dynamicIntensity = CHHapticDynamicParameter(parameterID: .hapticIntensityControl, value: Float(self.valueOfIntensity), relativeTime: 0)
-                    
-                    do {
-                        // Apply the dynamic parameter to the player
-                        try player.sendParameters([dynamicIntensity], atTime: 0)
-                    } catch {
-                        print("Error updating haptic intensity: \(error.localizedDescription)")
-                    }
-                    
-                    // Sleep for a short duration before updating again
-                    usleep(100_000) // Sleep for 100 milliseconds (adjust as needed)
+        func startVibration() {
+            guard let engine = engine else { return }
+
+            do {
+                try engine.start()
+
+                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(valueOfIntensity))
+                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.2)
+                let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: .infinity)
+
+                let pattern = try CHHapticPattern(events: [event], parameters: [])
+
+                let player = try engine.makePlayer(with: pattern)
+                try player.start(atTime: 0)
+
+                DispatchQueue.main.async {
+                    self.isVibrating = true
                 }
+
+
+            } catch {
+                print("Error playing haptic pattern: \(error.localizedDescription)")
             }
-            
-            
-        } catch {
-            print("Error playing haptic pattern: \(error.localizedDescription)")
         }
-    }
+    
     
     private func stopVibration() {
         guard let engine = engine else { return }
