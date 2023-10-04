e897d58803915d359943de8a7a0e172f12d88ee1
KarVarr <megatr9n@gmail.com>
2023-10-04 21:08:11 +0400

    SpaceGram added zoom for HD image

--- SpaceGram/SpaceGram/App/Controller/PictureOfDayScreen/HDPictureViewController/ScrollView + Ext.swift
+++ SpaceGram/SpaceGram/App/Controller/PictureOfDayScreen/HDPictureViewController/ScrollView + Ext.swift
@@ -9,70 +9,72 @@ import UIKit
 
 
 extension HDPictureViewController: UIScrollViewDelegate {
+    
     func configureScrollView() {
         scrollViewForImage.scroll.delegate = self
         
-//        scrollViewForImage.scroll.maximumZoomScale = 4
-//        scrollViewForImage.scroll.minimumZoomScale = 1
+        scrollViewForImage.scroll.maximumZoomScale = 3
+        scrollViewForImage.scroll.minimumZoomScale = 1
         scrollViewForImage.scroll.showsVerticalScrollIndicator = false
         scrollViewForImage.scroll.showsHorizontalScrollIndicator = false
         scrollViewForImage.scroll.decelerationRate = UIScrollView.DecelerationRate.fast
-        setCurrentMaxAndMinZoomScale()
+        
+        // Добавляем жест зумирования по двойному тапу
+        let zoomingTap = UITapGestureRecognizer(target: self, action: #selector(handleZoomingTap))
+        zoomingTap.numberOfTapsRequired = 2
+        hdPictureOfDay.customImage.addGestureRecognizer(zoomingTap)
+        hdPictureOfDay.customImage.isUserInteractionEnabled = true
+        
     }
+    
+    @objc func handleZoomingTap(sender: UITapGestureRecognizer) {
+           let location = sender.location(in: hdPictureOfDay.customImage)
+           
+           if scrollViewForImage.scroll.zoomScale > scrollViewForImage.scroll.minimumZoomScale {
+               scrollViewForImage.scroll.setZoomScale(scrollViewForImage.scroll.minimumZoomScale, animated: true)
+           } else {
+               let newScale = scrollViewForImage.scroll.maximumZoomScale
+               let zoomRect = zoomRectForScale(scale: newScale, center: location)
+               scrollViewForImage.scroll.zoom(to: zoomRect, animated: true)
+           }
+       }
+    func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+          let width = scrollViewForImage.scroll.frame.size.width / scale
+          let height = scrollViewForImage.scroll.frame.size.height / scale
+          let x = center.x - (width / 2.0)
+          let y = center.y - (height / 2.0)
+          return CGRect(x: x, y: y, width: width, height: height)
+      }
     
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return hdPictureOfDay.customImage
     }
+        
     
-    func setCurrentMaxAndMinZoomScale() {
-        let scrollView = scrollViewForImage.scroll
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
         
-        let boundsSize = scrollView.bounds.size
-        let imageSize = hdPictureOfDay.customImage.bounds.size
-        
-        let xScale = boundsSize.width / imageSize.width
-        let yScale = boundsSize.height / imageSize.height
-        let minScale = min(xScale, yScale)
-        
-        var maxScale: CGFloat = 1.0
-        if minScale < 0.1 {
-            maxScale = 0.3
+        if scrollView.zoomScale > 1 {
+            if let image = hdPictureOfDay.customImage.image {
+                let ratioWidth = hdPictureOfDay.customImage.frame.width / image.size.width
+                let ratioHeight = hdPictureOfDay.customImage.frame.height / image.size.height
+                
+                let ratio = ratioWidth < ratioHeight ? ratioWidth : ratioHeight
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                let conditionLeft = newWidth*scrollView.zoomScale > hdPictureOfDay.customImage.frame.width
+                let left =  (conditionLeft ? newWidth - hdPictureOfDay.customImage.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                let conditionTop = newHeight*scrollView.zoomScale > hdPictureOfDay.customImage.frame.height
+                
+                let top =  (conditionTop ? newHeight - hdPictureOfDay.customImage.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+                
+            }
+        } else {
+            scrollView.contentInset = .zero
         }
-        if minScale >= 0.1 && minScale < 0.5 {
-            maxScale = 0.7
-        }
-        if minScale >= 0.5 {
-            maxScale = max(1.0, minScale)
-        }
-        
-        scrollView.minimumZoomScale = minScale
-        scrollView.maximumZoomScale = maxScale
     }
-    
-//    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-//        
-//        if scrollView.zoomScale > 1 {
-//            if let image = hdPictureOfDay.customImage.image {
-//                let ratioWidth = hdPictureOfDay.customImage.frame.width / image.size.width
-//                let ratioHeight = hdPictureOfDay.customImage.frame.height / image.size.height
-//                
-//                let ratio = ratioWidth < ratioHeight ? ratioWidth : ratioHeight
-//                let newWidth = image.size.width * ratio
-//                let newHeight = image.size.height * ratio
-//                let conditionLeft = newWidth*scrollView.zoomScale > hdPictureOfDay.customImage.frame.width
-//                let left =  (conditionLeft ? newWidth - hdPictureOfDay.customImage.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
-//                let conditionTop = newHeight*scrollView.zoomScale > hdPictureOfDay.customImage.frame.height
-//                
-//                let top =  (conditionTop ? newHeight - hdPictureOfDay.customImage.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
-//                
-//                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
-//                
-//            }
-//        } else {
-//            scrollView.contentInset = .zero
-//        }
-//    }
     
     
 //    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
