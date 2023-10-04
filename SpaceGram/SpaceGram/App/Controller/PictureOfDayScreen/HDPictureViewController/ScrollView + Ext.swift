06523a8a716835f94a75441fe075e3ee61e1b216
KarVarr <megatr9n@gmail.com>
2023-10-04 21:23:56 +0400

    SpaceGram added single tap and double tap

--- SpaceGram/SpaceGram/App/Controller/PictureOfDayScreen/HDPictureViewController/ScrollView + Ext.swift
+++ SpaceGram/SpaceGram/App/Controller/PictureOfDayScreen/HDPictureViewController/ScrollView + Ext.swift
@@ -19,38 +19,59 @@ extension HDPictureViewController: UIScrollViewDelegate {
         scrollViewForImage.scroll.showsHorizontalScrollIndicator = false
         scrollViewForImage.scroll.decelerationRate = UIScrollView.DecelerationRate.fast
         
-        // Добавляем жест зумирования по двойному тапу
         let zoomingTap = UITapGestureRecognizer(target: self, action: #selector(handleZoomingTap))
         zoomingTap.numberOfTapsRequired = 2
         hdPictureOfDay.customImage.addGestureRecognizer(zoomingTap)
         hdPictureOfDay.customImage.isUserInteractionEnabled = true
         
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(handleSingleTap))
+        singleTap.numberOfTapsRequired = 1
+        scrollViewForImage.scroll.addGestureRecognizer(singleTap)
+        singleTap.require(toFail: zoomingTap)
+        
     }
     
+    //MARK: - UITapGestureRecognizer
     @objc func handleZoomingTap(sender: UITapGestureRecognizer) {
-           let location = sender.location(in: hdPictureOfDay.customImage)
-           
-           if scrollViewForImage.scroll.zoomScale > scrollViewForImage.scroll.minimumZoomScale {
-               scrollViewForImage.scroll.setZoomScale(scrollViewForImage.scroll.minimumZoomScale, animated: true)
-           } else {
-               let newScale = scrollViewForImage.scroll.maximumZoomScale
-               let zoomRect = zoomRectForScale(scale: newScale, center: location)
-               scrollViewForImage.scroll.zoom(to: zoomRect, animated: true)
-           }
-       }
+        let location = sender.location(in: hdPictureOfDay.customImage)
+        
+        if scrollViewForImage.scroll.zoomScale > scrollViewForImage.scroll.minimumZoomScale {
+            scrollViewForImage.scroll.setZoomScale(scrollViewForImage.scroll.minimumZoomScale, animated: true)
+        } else {
+            let newScale = scrollViewForImage.scroll.maximumZoomScale
+            let zoomRect = zoomRectForScale(scale: newScale, center: location)
+            scrollViewForImage.scroll.zoom(to: zoomRect, animated: true)
+        }
+    }
+    
+    
+    @objc func handleSingleTap(sender: UITapGestureRecognizer) {
+        if navigationController?.navigationBar.isHidden == true {
+            navigationController?.setNavigationBarHidden(false, animated: true)
+            tabBarController?.tabBar.isHidden = false
+        } else {
+            navigationController?.setNavigationBarHidden(true, animated: true)
+            tabBarController?.tabBar.isHidden = true
+        }
+    }
+    
+
+    
+    
+    //MARK: - Zoom scale
     func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
-          let width = scrollViewForImage.scroll.frame.size.width / scale
-          let height = scrollViewForImage.scroll.frame.size.height / scale
-          let x = center.x - (width / 2.0)
-          let y = center.y - (height / 2.0)
-          return CGRect(x: x, y: y, width: width, height: height)
-      }
+        let width = scrollViewForImage.scroll.frame.size.width / scale
+        let height = scrollViewForImage.scroll.frame.size.height / scale
+        let x = center.x - (width / 2.0)
+        let y = center.y - (height / 2.0)
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
     
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return hdPictureOfDay.customImage
     }
-        
+    
     
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         
@@ -75,13 +96,4 @@ extension HDPictureViewController: UIScrollViewDelegate {
             scrollView.contentInset = .zero
         }
     }
-    
-    
-//    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-//        if scale > scrollView.minimumZoomScale {
-//            UIView.animate(withDuration: 0.3) {
-//                scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
-//            }
-//        }
-//    }
 }
