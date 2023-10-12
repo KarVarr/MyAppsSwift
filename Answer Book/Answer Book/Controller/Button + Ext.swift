a416c1642910537eee49afa733128b9cc0e24029
KarVarr <megatr9n@gmail.com>
2023-10-12 07:48:07 +0400

    Answer Book  refactoring

--- Answer Book/Answer Book/Controller/Button + Ext.swift
+++ Answer Book/Answer Book/Controller/Button + Ext.swift
@@ -8,10 +8,5 @@
 import Foundation
 
 extension MainViewController {
-    //MARK: - Button
-    func configureShakeButton() {
-        shakeButton.button.titleLabel?.font = Helper.Font.noteworthyBold(with: dynamicFontSize(20))
-        shakeButton.button.layer.cornerRadius = shakeButton.button.frame.height / 2
-        shakeButton.button.addTarget(self, action: #selector(shakeButtonPressed), for: .touchUpInside)
-    }
+   
 }
--- Answer Book/Answer Book/Controller/ButtonPressed + Ext.swift
+++ Answer Book/Answer Book/Controller/ButtonPressed + Ext.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 extension MainViewController {
+    //MARK: - Button
+    func configureShakeButton() {
+        shakeButton.button.titleLabel?.font = Helper.Font.noteworthyBold(with: dynamicFontSize(20))
+        shakeButton.button.layer.cornerRadius = shakeButton.button.frame.height / 2
+        shakeButton.button.addTarget(self, action: #selector(shakeButtonPressed), for: .touchUpInside)
+    }
+    
     @objc func shakeButtonPressed () {
         // Reset animation
         answerViewBox.viewBox.layer.removeAllAnimations()
