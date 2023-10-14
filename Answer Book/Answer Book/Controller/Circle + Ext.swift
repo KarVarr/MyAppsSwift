1b712d267a9d987cb7ed698aed3d633c37db99cc
KarVarr <megatr9n@gmail.com>
2023-10-15 02:01:38 +0400

    Answer Book  refactoring

---
+++ Answer Book/Answer Book/Controller/Circle + Ext.swift
@@ -0,0 +1,37 @@
+//
+//  Circle + Ext.swift
+//  Answer Book
+//
+//  Created by Karen Vardanian on 15.10.2023.
+//
+
+import UIKit
+
+
+extension MainViewController {
+    //MARK: - Circles
+    func createCircle(for circle: ViewBoxView, withColor color: UIColor) {
+        circle.viewBox.backgroundColor = .clear
+        
+        let circleLayer = CALayer()
+        circleLayer.bounds = circle.viewBox.bounds
+        circleLayer.position = CGPoint(x: circle.viewBox.bounds.midX, y: circle.viewBox.bounds.midY)
+        circleLayer.cornerRadius = circle.viewBox.bounds.width / 2
+        circleLayer.backgroundColor = color.cgColor
+        circleLayer.masksToBounds = true
+        
+        circle.viewBox.layer.addSublayer(circleLayer)
+    }
+    
+    //MARK: - Dynamic circle ball width for iPad and iPhone
+    func dynamicWidth() -> Double {
+        var width = 0.0
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            width = 0.45
+        } else if UIDevice.current.userInterfaceIdiom == .pad {
+            width = 0.35
+        }
+        return width
+    }
+
+}
--- Answer Book/Answer Book/Controller/Settings + Ext .swift
+++ Answer Book/Answer Book/Controller/Settings + Ext .swift
@@ -17,19 +17,7 @@ extension MainViewController {
         answerLabel.label.font = UIFont.systemFont(ofSize: dynamicFontSize(72))
     }
     
-    //MARK: - Circles
-    func createCircle(for circle: ViewBoxView, withColor color: UIColor) {
-        circle.viewBox.backgroundColor = .clear
-        
-        let circleLayer = CALayer()
-        circleLayer.bounds = circle.viewBox.bounds
-        circleLayer.position = CGPoint(x: circle.viewBox.bounds.midX, y: circle.viewBox.bounds.midY)
-        circleLayer.cornerRadius = circle.viewBox.bounds.width / 2
-        circleLayer.backgroundColor = color.cgColor
-        circleLayer.masksToBounds = true
-        
-        circle.viewBox.layer.addSublayer(circleLayer)
-    }
+    
     
     //MARK: - Dynamic Font size
     func dynamicFontSize(_ FontSize: CGFloat) -> CGFloat {
@@ -45,15 +33,5 @@ extension MainViewController {
         return dateFormatter.string(from: Date())
     }
     
-    //MARK: - Dynamic circle ball width for iPad and iPhone
-    func dynamicWidth() -> Double {
-        var width = 0.0
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            width = 0.45
-        } else if UIDevice.current.userInterfaceIdiom == .pad {
-            width = 0.35
-        }
-        return width
-    }
   
 }
