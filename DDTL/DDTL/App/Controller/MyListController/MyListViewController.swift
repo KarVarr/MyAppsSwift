556e3867bd43e7476256a768d832cf62676f3372
KarVarr <megatr9n@gmail.com>
2023-11-01 23:11:46 +0400

    DDTL added tableView

--- DDTL/DDTL/App/Controller/MyListController/MyListViewController.swift
+++ DDTL/DDTL/App/Controller/MyListController/MyListViewController.swift
@@ -8,15 +8,33 @@
 import UIKit
 
 class MyListViewController: BaseViewController {
-
+    let tableView = UITableViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
         
         setNavTitle(title: .myListViewController)
+        
+        tableView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
 
+}
+
+extension MyListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "number - \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 }
 
 
