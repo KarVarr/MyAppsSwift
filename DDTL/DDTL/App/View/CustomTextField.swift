6a12b4fef579d20c59fd4c8a520a8494f168ac03
KarVarr <megatr9n@gmail.com>
2023-11-04 13:36:08 +0400

    DDTL added customTextField

---
+++ DDTL/DDTL/App/View/CustomTextField.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTextField.swift
+//  DDTL
+//
+//  Created by Karen Vardanian on 04.11.2023.
+//
+
+import UIKit
+
+class CustomTextField {
+    let textField: UITextField = {
+       let text = UITextField()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+}
