bd951f4101fa1406deb0e1c6d7d9a0bced813c92
KarVarr <megatr9n@gmail.com>
2024-01-20 13:16:04 +0400

    Tomato Timer add clock and timer

---
+++ Tomato Timer/Tomato Timer/Clock.swift
@@ -0,0 +1,20 @@
+//
+//  Clock.swift
+//  Tomato Timer
+//
+//  Created by Karen Vardanian on 20.01.2024.
+//
+
+import SwiftUI
+
+struct Clock: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Clock()
+}
---
+++ Tomato Timer/Tomato Timer/Timer.swift
@@ -0,0 +1,116 @@
+//
+//  Timer.swift
+//  Tomato Timer
+//
+//  Created by Karen Vardanian on 19.01.2024.
+//
+
+import SwiftUI
+import Foundation
+
+
+struct Timer: View {
+    let defaultTime: CGFloat = 25
+//    @State private var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
+
+    @State private var timerRunning = false
+    @State private var countdownTime: CGFloat = 25
+    @State private var currentDate: String = ""
+
+    var strokeStyle: StrokeStyle {
+        StrokeStyle(lineWidth: 15, lineCap: .round)
+    }
+
+    var buttonIcon: String {
+        timerRunning ? "pause.rectangle.fill" : "play.rectangle.fill"
+    }
+
+    var countdownColor: Color {
+        switch countdownTime {
+        case 6...: return Color.green
+        case 3...: return Color.yellow
+        default: return Color.red
+        }
+    }
+    
+    
+    
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                
+                HStack {
+                    Text("struct")
+                        .foregroundStyle(.purple)
+                    Text("AppName {")
+                        .foregroundStyle(.cyan)
+                }
+                HStack {
+                    Text("      var")
+                        .foregroundStyle(.orange)
+                    Text("currentDate: ")
+                        .foregroundStyle(.blue)
+                    Text("\(currentDate)")
+                        .foregroundStyle(.yellow)
+                    
+                }
+                
+                Text("""
+            struct AppName {
+                var currentDate: \(currentDate)
+            }
+            var time: PM = 22:01
+            var battery: Normal = 56%
+            
+            """
+                )
+                .foregroundStyle(.pink)
+                .onAppear(perform: {
+                     
+                })
+            }
+            ZStack {
+                RoundedRectangle(cornerSize: CGSize(width: 70, height: 70), style: .continuous)
+                    .stroke(Color.gray.opacity(0.2), style: strokeStyle)
+                
+                RoundedRectangle(cornerSize: CGSize(width: 30, height: 30), style: .continuous)
+                    .trim(from: 0, to: 1 - ((defaultTime - countdownTime) / defaultTime))
+                    .stroke(countdownColor, style: strokeStyle)
+                    .rotationEffect(.degrees(-90))
+                    .animation(.easeInOut)
+                
+                HStack(spacing: 25) {
+                    Label("", systemImage: buttonIcon)
+                        .foregroundStyle(.black).font(.title)
+                        .onTapGesture(perform: {
+                            timerRunning.toggle()
+                        })
+                    Text("\(Int(countdownTime))")
+                        .font(.largeTitle)
+                    Label("", systemImage: "gobackward")
+                        .foregroundStyle(.red)
+                        .onTapGesture(perform: {
+                            timerRunning = false
+                            countdownTime = defaultTime
+                        })
+                }
+            }
+            .frame(width: 300, height: 300)
+//            .onReceive(timer, perform: { _ in
+//                guard timerRunning else { return }
+//                if countdownTime > 0 {
+//                    countdownTime -= 1
+//                } else {
+//                    timerRunning = false
+//                    countdownTime = defaultTime
+//                }
+//            })
+        }
+    }
+}
+
+#Preview {
+    Timer()
+}
