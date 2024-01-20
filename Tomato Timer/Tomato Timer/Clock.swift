422b75f52b893c00aa5e9473adead47b9029f650
KarVarr <megatr9n@gmail.com>
2024-01-20 15:38:04 +0400

    Tomato Timer added battery status

--- Tomato Timer/Tomato Timer/Clock.swift
+++ Tomato Timer/Tomato Timer/Clock.swift
@@ -7,8 +7,28 @@
 
 import SwiftUI
 
-struct Clock: View {
+class BatteryViewModel: ObservableObject {
+    
+}
 
+
+struct Clock: View {
+    enum TimeAMPM {
+        case pm
+        case am
+    }
+    
+ 
+    
+    @State private var batteryLevel: Float = UIDevice.current.batteryLevel
+    @State private var currentTimePeriod: TimeAMPM
+    
+    init() {
+        let currentDate = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: currentDate)
+        _currentTimePeriod = State(initialValue: hour < 12 ? .am : .pm)
+    }
     
     
     var body: some View {
@@ -34,21 +54,48 @@ struct Clock: View {
                     HStack {
                         textAndColor(name: "        var", color: Helper.Colors.variable)
                         Text("\(textAndColor(name: "time", color: Helper.Colors.variableName))\(textAndColor(name: ":", color: Helper.Colors.brackets))")
-                        textAndColor(name: "PM", color: Helper.Colors.type)
+                        
+                        switch currentTimePeriod {
+                        case .am: textAndColor(name: "AM", color: Helper.Colors.type)
+                        case .pm: textAndColor(name: "PM", color: Helper.Colors.type)
+                        }
+                        
                         textAndColor(name: "=", color: Helper.Colors.brackets)
                         textAndColor(name: "\(getCurrentTime())", color: Helper.Colors.number)
                     }
-                    
-//                    Text("""
-//        var \(textAndColor(name:"time", color: Color(uiColor: Helper.Colors.type)))\(textAndColor(name:":",color: Color(uiColor: Helper.Colors.brackets))) PM = 22:01
-//        var battery: Normal = 56%
-//        
-//        """
-                    
-                   
-                    .onAppear(perform: {
+                    //MARK: - Battery
+                    HStack {
                         
-                    })
+                        textAndColor(name: "        var", color: Helper.Colors.variable)
+                        Text("\(textAndColor(name: "battery", color: Helper.Colors.variableName))\(textAndColor(name: ":", color: Helper.Colors.brackets))")
+                        
+                        switch batteryLevel {
+                        case 0.0..<20.0: textAndColor(name: "Low", color: Helper.Colors.type)
+                        case 20.0..<60.0: textAndColor(name: "Medium", color: Helper.Colors.type)
+                        case 60.0..<100.0: textAndColor(name: "Good", color: Helper.Colors.type)
+                        default:
+                            textAndColor(name: "N/A", color: Helper.Colors.type)
+                        }
+                    
+                        textAndColor(name: "=", color: Helper.Colors.brackets)
+                        textAndColor(name: String(format: "%.0f", batteryLevel * 100), color: Helper.Colors.number)
+                    }
+                    //MARK: - End of struct
+                    textAndColor(name: "}", color: Helper.Colors.brackets)
+                        .onAppear {
+                            UIDevice.current.isBatteryMonitoringEnabled = true
+                            
+                            NotificationCenter.default.addObserver(
+                                forName: UIDevice.batteryLevelDidChangeNotification,
+                                object: nil,
+                                queue: nil
+                            ) { _ in
+                                batteryLevel = UIDevice.current.batteryLevel
+                            }
+                        }
+                        .onDisappear {
+                            UIDevice.current.isBatteryMonitoringEnabled = false
+                        }
                 }
             }
     }
@@ -65,12 +112,12 @@ struct Clock: View {
     
     func getCurrentTime() -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.timeStyle = .medium
+        dateFormatter.dateFormat = "h:mm"
         dateFormatter.timeZone = TimeZone.current
         dateFormatter.locale = Locale.current
         return dateFormatter.string(from: Date())
     }
-
+    
     
     func textAndColor(name: String, color: UIColor) -> Text {
         return Text(name)
