d62b4b78750d01001c2febfae4cd5794d52177ce
KarVarr <megatr9n@gmail.com>
2024-01-20 16:35:11 +0400

    Tomato Timer added battery model

--- Tomato Timer/Tomato Timer/Clock.swift
+++ Tomato Timer/Tomato Timer/Clock.swift
@@ -8,7 +8,51 @@
 import SwiftUI
 
 class BatteryViewModel: ObservableObject {
+    @Published var batteryLevel: Int = 0
+    @Published var batteryStateDescription: String = ""
+    @Published var batteryStateColor: UIColor = Helper.Colors.type
     
+    init() {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        self.batteryLevel = Int(UIDevice.current.batteryLevel * 100)
+        getBatteryState()
+    }
+    
+    private func getBatteryState() {
+        let batteryState = UIDevice.current.batteryState
+        self.batteryStateDescription = getBatteryDescription(for: batteryState)
+        self.batteryStateColor = getBatteryColor(for: batteryState)
+    }
+    
+    private func getBatteryColor(for state: UIDevice.BatteryState) -> UIColor {
+        switch state {
+        case .unknown:
+            return Helper.Colors.type
+        case .unplugged:
+            return Helper.Colors.type
+        case .charging:
+            return Helper.Colors.ifElseCondition
+        case .full:
+            return Helper.Colors.type
+        @unknown default:
+            return Helper.Colors.type
+        }
+    }
+    
+    private func getBatteryDescription(for state: UIDevice.BatteryState) -> String {
+        switch state {
+        case .unknown:
+            return "Unknown"
+        case .unplugged:
+            return "On battery power"
+        case .charging:
+            return "Charging"
+        case .full:
+            return "Full"
+        @unknown default:
+            return "Unknown"
+        }
+    }
 }
 
 
