5f103a46b46903ca596f929899889ae843929256
KarVarr <megatr9n@gmail.com>
2024-01-21 12:20:27 +0400

    Tomato Timer added seconds and minuts for timer

--- Tomato Timer/Tomato Timer/View/TimerView.swift
+++ Tomato Timer/Tomato Timer/View/TimerView.swift
@@ -10,11 +10,11 @@ import Foundation
 
 
 struct TimerView: View {
-    let defaultTime: CGFloat = 1500
+    let defaultTime: CGFloat = 320
     var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     @State private var timerRunning = false
-    @State private var countdownTime: CGFloat = 1500
+    @State private var countdownTime: CGFloat = 320
     
     var strokeStyle: StrokeStyle {
         StrokeStyle(lineWidth: 15, lineCap: .round)
@@ -32,54 +32,56 @@ struct TimerView: View {
         }
     }
     
+    var countdownMinutes: Int {
+        return Int(countdownTime / 60)
+    }
     
+    var countdownSeconds: Int {
+        return Int(countdownTime) % 60
+    }
     
     
     var body: some View {
         Color(uiColor: Helper.Colors.background)
             .ignoresSafeArea()
             .overlay {
-              
-                    ZStack {
-                        Circle()
-                            .stroke(Color.gray.opacity(0.2), style: strokeStyle)
-                        
-                        Circle()
-                            .trim(from: 0, to: 1 - ((defaultTime - countdownTime) / defaultTime))
-                            .stroke(countdownColor, style: strokeStyle)
-                            .rotationEffect(.degrees(-90))
-                            .animation(.easeIn, value: countdownTime)
-                        
-                        HStack(spacing: 25) {
-                            Label("", systemImage: buttonIcon)
-                                .foregroundStyle(Color(uiColor: Helper.Colors.variable)).font(.largeTitle)
-                                .onTapGesture(perform: {
-                                    timerRunning.toggle()
-                                })
-                            Text("\(Int(countdownTime))")
-                                .foregroundStyle(Color(uiColor: Helper.Colors.brackets)) .font(.largeTitle)
-                            Label("", systemImage: "gobackward")
-                                .foregroundStyle(Color(uiColor: Helper.Colors.string)).font(.largeTitle)
-                                .onTapGesture(perform: {
-                                    timerRunning = false
-                                    countdownTime = defaultTime
-                                })
-                        }
+                
+                ZStack {
+                    Circle()
+                        .stroke(Color.gray.opacity(0.2), style: strokeStyle)
+                    
+                    Circle()
+                        .trim(from: 0, to: 1 - ((defaultTime - countdownTime) / defaultTime))
+                        .stroke(countdownColor, style: strokeStyle)
+                        .rotationEffect(.degrees(-90))
+                        .animation(.easeIn, value: countdownTime)
+                    
+                    HStack(spacing: 25) {
+                        Label("", systemImage: buttonIcon)
+                            .foregroundStyle(Color(uiColor: Helper.Colors.variable)).font(.largeTitle)
+                            .onTapGesture(perform: {
+                                timerRunning.toggle()
+                            })
+                        Text(String(format: "%02d:%02d", countdownMinutes, countdownSeconds))
+                            .foregroundStyle(Color(uiColor: Helper.Colors.brackets)) .font(.largeTitle.monospacedDigit())
+                        Label("", systemImage: "gobackward")
+                            .foregroundStyle(Color(uiColor: Helper.Colors.string)).font(.largeTitle)
+                            .onTapGesture(perform: {
+                                timerRunning = false
+                                countdownTime = defaultTime
+                            })
                     }
-                    .frame(width: 300, height: 300)
-                    .onReceive(timer, perform: { _ in
-                        guard timerRunning else { return }
-                        if countdownTime > 0 {
-                            countdownTime -= 1
-                        } else {
-                            timerRunning = false
-                            countdownTime = defaultTime
-                        }
-                    })
-                    
-                    
-                
-                
+                }
+                .frame(width: 300, height: 300)
+                .onReceive(timer, perform: { _ in
+                    guard timerRunning else { return }
+                    if countdownTime > 0 {
+                        countdownTime -= 1
+                    } else {
+                        timerRunning = false
+                        countdownTime = defaultTime
+                    }
+                })
                 
             }
     }
