310913e1f0368f9e8c05f202816b681aeb00d231
KarVarr <megatr9n@gmail.com>
2024-03-08 11:55:39 +0400

    Tomato Timer added new state for contertView to cheanging timer in case of Mood

--- Tomato Timer/Tomato Timer/View/Extention/TimerView + Ext.swift
+++ Tomato Timer/Tomato Timer/View/Extention/TimerView + Ext.swift
@@ -26,7 +26,9 @@ extension TimerView {
                 timerRunning = false
                 countdownTime = defaultTime
                 timerCount = countdownTime
-                sessionCount += 1
+                if currentMood == .code {
+                    sessionCount += 1
+                }
             }
         }
     }
@@ -39,42 +41,66 @@ extension TimerView {
         timerRunning = false
         countdownTime = defaultTime
 
-        
-        
-        if adding {
-            if defaultTime >= 3600 {
-                isPlusButtonOnOff = true
-                isMinusButtonOnOff = false
-                defaultTime = 3600
-                countdownTime = 3600
-                timerCount = countdownTime
-            } else {
-                isMinusButtonOnOff = false
-                isPlusButtonOnOff = defaultTime + 300 >= 3600
-                defaultTime += 300
-                countdownTime += 300
-                timerCount = countdownTime
-            }
-        } else {
-            if defaultTime <= 1200 {
-                isMinusButtonOnOff = true
-                isPlusButtonOnOff = false
-                defaultTime = 1200
-                countdownTime = 1200
-                timerCount = countdownTime
-            } else {
-                isPlusButtonOnOff = false
-                isMinusButtonOnOff = defaultTime - 300 <= 1200
-                defaultTime -= 300
-                countdownTime -= 300
-                timerCount = countdownTime
-            }
-        }
-        
         if currentMood == .code {
-            timerCount = 1200
+            if adding {
+                if defaultTime >= 3600 {
+                    isPlusButtonOnOff = true
+                    isMinusButtonOnOff = false
+                    defaultTime = 3600
+                    countdownTime = 3600
+                    timerCount = countdownTime
+                } else {
+                    isMinusButtonOnOff = false
+                    isPlusButtonOnOff = defaultTime + 300 >= 3600
+                    defaultTime += 300
+                    countdownTime += 300
+                    timerCount = countdownTime
+                }
+            } else {
+                if defaultTime <= 1200 {
+                    isMinusButtonOnOff = true
+                    isPlusButtonOnOff = false
+                    defaultTime = 1200
+                    countdownTime = 1200
+                    timerCount = countdownTime
+                } else {
+                    isPlusButtonOnOff = false
+                    isMinusButtonOnOff = defaultTime - 300 <= 1200
+                    defaultTime -= 300
+                    countdownTime -= 300
+                    timerCount = countdownTime
+                }
+            }
         } else {
-            timerCount = CGFloat(300 + (15 - countdownMinutes) * 60)
+            if adding {
+                if defaultTime >= 900 {
+                    isPlusButtonOnOff = true
+                    isMinusButtonOnOff = false
+                    defaultTime = 900
+                    countdownTime = 900
+                    timerCount = countdownTime
+                } else {
+                    isMinusButtonOnOff = false
+                    isPlusButtonOnOff = defaultTime + 300 >= 900
+                    defaultTime += 300
+                    countdownTime += 300
+                    timerCount = countdownTime
+                }
+            } else {
+                if defaultTime <= 300 {
+                    isMinusButtonOnOff = true
+                    isPlusButtonOnOff = false
+                    defaultTime = 300
+                    countdownTime = 300
+                    timerCount = countdownTime
+                } else {
+                    isPlusButtonOnOff = false
+                    isMinusButtonOnOff = defaultTime - 300 <= 300
+                    defaultTime -= 300
+                    countdownTime -= 300
+                    timerCount = countdownTime
+                }
+            }
         }
     }
 }
