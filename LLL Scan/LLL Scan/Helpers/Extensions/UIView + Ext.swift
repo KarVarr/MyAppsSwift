83e079d8e04a6399a4771d240c49b986d5b072dd
KarVarr <megatr9n@gmail.com>
2024-04-03 21:20:14 +0400

    LLL Scan added new func for UIVIew to use it for install a tamic for default every view

---
+++ LLL Scan/LLL Scan/Helpers/Extensions/UIView + Ext.swift
@@ -0,0 +1,14 @@
+//
+//  UIView + Ext.swift
+//  LLL Scan
+//
+//  Created by Karen Vardanian on 03.04.2024.
+//
+
+import UIKit
+
+extension UIView {
+    func setupTamic() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ LLL Scan/LLL Scan/View/ButtonView.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonView.swift
+//  LLL Scan
+//
+//  Created by Karen Vardanian on 03.04.2024.
+//
+
+import UIKit
