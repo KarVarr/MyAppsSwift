6d1c84cf85fd08df9d5c15c395dcebae57b8994d
KarVarr <megatr9n@gmail.com>
2024-04-14 20:28:49 +0400

    LLL Scan added gitignore and google api

---
+++ LLL Scan/LLL Scan/API/myApi.swift
@@ -0,0 +1,14 @@
+//
+//  myApi.swift
+//  LLL Scan
+//
+//  Created by Karen Vardanian on 14.04.2024.
+//
+
+import UIKit
+
+enum API {
+    enum Key {
+        static let googleTranslateKey = "test"
+    }
+}
