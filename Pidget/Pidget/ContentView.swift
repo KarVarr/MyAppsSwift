27d48074029d3192f6718cca8bd88a74aa329abf
KarVarr <megatr9n@gmail.com>
2024-04-15 20:59:05 +0400

    added new project Pidget

---
+++ Pidget/Pidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pidget
+//
+//  Created by Karen Vardanian on 15.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pidget/Pidget/PidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PidgetApp.swift
+//  Pidget
+//
+//  Created by Karen Vardanian on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
