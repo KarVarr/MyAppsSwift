10893ed988194db9982c787509b2f72603a85a8c
KarVarr <megatr9n@gmail.com>
2024-04-28 17:10:09 +0400

    LLL Scan change launchScreen

--- LLL Scan/LLL Scan/App/Controller/ProductDetailViewController/ConfigureViewPDVC + Ext.swift
+++ LLL Scan/LLL Scan/App/Controller/ProductDetailViewController/ConfigureViewPDVC + Ext.swift
@@ -5,7 +5,7 @@
 //  Created by Karen Vardanian on 24.04.2024.
 //
 
-import Foundation
+import UIKit
 
 extension ProductDetailVC {
     override func configureView() {
@@ -13,5 +13,11 @@ extension ProductDetailVC {
         title = "Карточки товара"
         
         navigationController?.navigationBar.prefersLargeTitles = true
+        let convertFileToCVSButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up.fill"), style: .plain, target: self, action: #selector(shareButtonToConvertFile))
+        navigationItem.rightBarButtonItem = convertFileToCVSButton
+    }
+    
+    @objc func shareButtonToConvertFile() {
+        print("convert file done")
     }
 }
