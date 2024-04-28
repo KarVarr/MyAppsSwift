e05f37adae5680e094a1c283a7c9fc6ea7ff0558
KarVarr <megatr9n@gmail.com>
2024-04-28 14:03:51 +0400

    LLL Scan added a count label and a button for the Scan tablecell

--- LLL Scan/LLL Scan/App/View/CustomTableView/ScanTableViewCell.swift
+++ LLL Scan/LLL Scan/App/View/CustomTableView/ScanTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ScanTableViewCell: UITableViewCell {
     let titleLabel = LabelViewCustom()
+    let countLabel = LabelViewCustom()
+    let buttonConvertToCSV = ButtonViewCustom()
+    let vStackSTVCForLabelsTitleAndCount = ViewStackCustom()
+    let hStackSTVCForVStackAndButton = ViewStackCustom()
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,15 +25,21 @@ class ScanTableViewCell: UITableViewCell {
         addViews()
         configureCell()
         configureLabels()
+        configureStacks()
         layoutCell()
         
     }
     
     
     private func addViews() {
-        contentView.addSubview(titleLabel.label)
+        contentView.addSubview(vStackSTVCForLabelsTitleAndCount.stack)
+        contentView.addSubview(hStackSTVCForVStackAndButton.stack)
+        
+        vStackSTVCForLabelsTitleAndCount.stack.addArrangedSubview(titleLabel.label)
+        vStackSTVCForLabelsTitleAndCount.stack.addArrangedSubview(countLabel.label)
+        hStackSTVCForVStackAndButton.stack.addArrangedSubview(vStackSTVCForLabelsTitleAndCount.stack)
+        hStackSTVCForVStackAndButton.stack.addArrangedSubview(buttonConvertToCSV.button)
     }
-    
     private func configureCell() {
         
     }
@@ -37,10 +48,28 @@ class ScanTableViewCell: UITableViewCell {
         
     }
     
+    private func configureStacks() {
+        vStackSTVCForLabelsTitleAndCount.stack.axis = .vertical
+        vStackSTVCForLabelsTitleAndCount.stack.distribution = .fillEqually
+        vStackSTVCForLabelsTitleAndCount.stack.spacing = 5
+        vStackSTVCForLabelsTitleAndCount.stack.alignment = .leading
+        
+        hStackSTVCForVStackAndButton.stack.axis = .horizontal
+        hStackSTVCForVStackAndButton.stack.distribution = .fillEqually
+        hStackSTVCForVStackAndButton.stack.alignment = .center
+    }
+    
     private func layoutCell() {
+        let vStackSTVCForLabelsTitleAndCount = vStackSTVCForLabelsTitleAndCount.stack
+        let hStackSTVCForVStackAndButton = hStackSTVCForVStackAndButton.stack
+        
         NSLayoutConstraint.activate([
-            titleLabel.label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            titleLabel.label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            hStackSTVCForVStackAndButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            hStackSTVCForVStackAndButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            hStackSTVCForVStackAndButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            
+            vStackSTVCForLabelsTitleAndCount.leadingAnchor.constraint(equalTo: hStackSTVCForVStackAndButton.leadingAnchor, constant: 20),
+            vStackSTVCForLabelsTitleAndCount.centerYAnchor.constraint(equalTo: hStackSTVCForVStackAndButton.centerYAnchor),
         ])
     }
     
