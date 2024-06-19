b0b36562124e153a811c3ea49069cf352423f3ac
KarVarr <megatr9n@gmail.com>
2024-06-19 21:28:51 +0400

    LLL Scan added skeleton

--- LLL Scan/LLL Scan/App/Controller/ProductDetailsViewController/TableViewHeader.swift
+++ LLL Scan/LLL Scan/App/Controller/ProductDetailsViewController/TableViewHeader.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
+import SkeletonView
 
 class TableViewHeader: UIView {
-    let activityIndicator = ActivityIndicatorCustom()
     
     let imageView: UIImageView = {
         let imageView = UIImageView()
@@ -20,13 +20,19 @@ class TableViewHeader: UIView {
     
     init(frame: CGRect, imgURL: String) {
         super.init(frame: frame)
+        setupView()
         configureImageView(with: imgURL)
         setupConstraints()
     }
     
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.isSkeletonable = true // Make the entire view skeletonable
     }
     
     private func configureImageView(with urlString: String) {
@@ -36,18 +42,22 @@ class TableViewHeader: UIView {
             imageView.image = UIImage(named: "photo")
         }
     }
+    // MARK: - Start Skeleton
+    private func startSkeleton() {
+        self.showAnimatedGradientSkeleton()
+    }
+    
+    // MARK: - Stop Skeleton
+    private func stopSkeleton() {
+        self.hideSkeleton()
+    }
     
     private func loadImage(from url: URL) {
-        imageView.isHidden = true
-        activityIndicator.indicatorView.isHidden = false
-        activityIndicator.indicatorView.startAnimating()
-        
+        startSkeleton()
         DispatchQueue.global().async { [weak self] in
             if let imageData = try? Data(contentsOf: url), let image = UIImage(data: imageData) {
                 DispatchQueue.main.async {
-                    self?.activityIndicator.indicatorView.isHidden = true
-                    self?.activityIndicator.indicatorView.stopAnimating()
-                    self?.imageView.isHidden = false
+                    self?.stopSkeleton()
                     self?.imageView.image = image
                 }
             }
@@ -56,15 +66,10 @@ class TableViewHeader: UIView {
     
     private func setupConstraints() {
         addSubview(imageView)
-        addSubview(activityIndicator.indicatorView)
-        
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             imageView.heightAnchor.constraint(equalToConstant: 400),
-            
-            activityIndicator.indicatorView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
-            activityIndicator.indicatorView.centerYAnchor.constraint(equalTo: imageView.centerYAnchor),
             
         ])
     }
