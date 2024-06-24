29c32d2b242a90b54c579a8fc85faaa97d922a08
KarVarr <megatr9n@gmail.com>
2024-06-24 07:32:55 +0400

    LLL Scan added repo xlsxwriter for making an excel file

--- LLL Scan/LLL Scan/App/Controller/ListOfProductsViewController/ConfigureViewLPVC + Ext.swift
+++ LLL Scan/LLL Scan/App/Controller/ListOfProductsViewController/ConfigureViewLPVC + Ext.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import CoreXLSX
+import xlsxwriter
 
 extension ListOfProductsVC {
     override func configureView() {
@@ -17,32 +17,66 @@ extension ListOfProductsVC {
         let convertFileToCSVButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up.fill"), style: .plain, target: self, action: #selector(shareButtonToConvertFile))
         navigationItem.rightBarButtonItem = convertFileToCSVButton
     }
-        
+    
+    
     @objc func shareButtonToConvertFile() {
-        print("convert file done")
+        print("Convert file to XLSX")
         
         guard let productListIndex = productListIndex else {
             print("productListIndex is nil")
             return
         }
         
-        let file_name = "H&M articuls.csv"
-        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file_name)
-        var csvHead = "Article,Title,Price,Color,Description,Material,link,ImageLink\n"
-        
         let productsContainer = dataManager.allProducts[productListIndex]
         
+        // Создание нового XLSX файла
+        let tempDirectory = NSTemporaryDirectory()
+        let fileName = "Products.xlsx"
+        let tempPath = NSURL(fileURLWithPath: tempDirectory).appendingPathComponent(fileName)
+        
+        
+        let document = Workbook(name: tempPath!.path)
+        let sheet = document.addWorksheet(name: "Products")
+        
+        // Добавление заголовков
+        sheet.write("Article", [0, 0])
+        sheet.write("Title", [0, 1])
+        sheet.write("Price", [0, 2])
+        sheet.write("Color", [0, 3])
+        sheet.write("Description", [0, 4])
+        sheet.write("Material", [0, 5])
+        sheet.write("Link", [0, 6])
+        sheet.write("ImageLink", [0, 7])
+        
+        // Добавление данных
+        var row = 1
         for product in productsContainer {
-            csvHead.append("\(String(describing: product.article ?? "")),\(String(describing: product.title ?? "")),\(String(describing: product.price ?? "")),\(String(describing: product.colorName ?? "")),\(String(describing: product.descriptions ?? "")),\(String(describing: product.material ?? "")),\(String(describing: product.link ?? "")),\(String(describing: product.imageURL ?? ""))\n")
+            sheet.write(.string(product.article ?? ""), [row, 0])
+            sheet.write(.string(product.title ?? ""), [row, 1])
+            sheet.write(.string(product.price ?? ""), [row, 2])
+            sheet.write(.string(product.colorName ?? ""), [row, 3])
+            sheet.write(.string(product.descriptions ?? ""), [row, 4])
+            sheet.write(.string(product.material ?? ""), [row, 5])
+            sheet.write(.string("https://www2.hm.com\(product.link ?? "")"), [row, 6])
+            sheet.write(.string("https:\(product.mainImageURL  ?? "")"), [row, 7])
+            
+            row += 1
         }
         
-        do {
-            try csvHead.write(to: path!, atomically: true, encoding: .utf8)
-            let exportSheet = UIActivityViewController(activityItems: [path as Any], applicationActivities: nil)
+        // Закрытие и сохранение документа
+        document.close()
+        
+        // Проверка существования файла и его передача через UIActivityViewController
+        if FileManager.default.fileExists(atPath: tempPath!.path) {
+            let fileURL = URL(fileURLWithPath: tempPath!.path)
+            let exportSheet = UIActivityViewController(activityItems: [fileURL], applicationActivities: nil)
             self.present(exportSheet, animated: true)
-        } catch  {
-            print("Error CSV ------>>>>>>>")
+        } else {
+            print("Error: File could not be saved")
         }
     }
-    
 }
+
+
+
+
