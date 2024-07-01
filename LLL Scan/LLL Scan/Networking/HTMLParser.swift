28416a77f7a859d23d28dc195a86215eba7c3eb4
KarVarr <megatr9n@gmail.com>
2024-07-01 23:37:12 +0400

    LLL Scan fixed color

--- LLL Scan/LLL Scan/Networking/HTMLParser.swift
+++ LLL Scan/LLL Scan/Networking/HTMLParser.swift
@@ -1,15 +1,9 @@
-//
-//  HTMLParser.swift
-//  LLL Scan
-//
-//  Created by Karen Vardanian on 31.03.2024.
-//
-
 import Foundation
 import SwiftSoup
 
 enum ParserError: Error {
     case invalidHTML
+    case jsonParsingFailed
 }
 
 class HTMLParser {
@@ -28,7 +22,7 @@ class HTMLParser {
         case _ where input.contains("Girls") && input.contains("Baby"):
             return "Девочка"
         default:
-            return "Нет данных на сайте"
+            return "---"
         }
     }
     
@@ -39,28 +33,33 @@ class HTMLParser {
             guard let body = doc.body() else {
                 return .failure(ParserError.invalidHTML)
             }
-            
+            //MARK: - MINI IMG
             let aElement = try body.select("div[class='fcc68c a33b36 f6e252']").first()
             let imgElement = try aElement?.select("img").first()
             let imgSrc = try imgElement?.attr("src").replacingOccurrences(of: "2160", with: "116")
             print("Image src: \(imgSrc ?? "N/A")")
             
+            //MARK: - FULLHD MAIN IMG
             let mainImageElement = try body.select("div[class='fcc68c a33b36 f6e252']").first()
             let mainImgElement = try mainImageElement?.select("img").first()
             let mainImgSrc = try mainImgElement?.attr("src")
             print("Main Image src: \(mainImgSrc ?? "N/A")")
             
+            //MARK: - ARTICLE
             let articleElement = try body.select("span[class='d1cd7b b7f566 a0f363']").first()
             let article = try articleElement?.text()
             print("Article: \(article ?? "N/A")")
             
+            //MARK: - LINK
             let link = "https://www2.hm.com/en_gb/productpage.\(article ?? "00000000").html"
             print("link: \(link)")
             
+            //MARK: - TITLE
             let titleElement = try body.select("h1").first()
             let title = try titleElement?.text()
             print("Title: \(title ?? "N/A")")
             
+            //MARK: - PRICE
             let priceElements = try body.select("div[class='e26896'] span")
             var price: String?
             for priceElement in priceElements {
@@ -76,27 +75,46 @@ class HTMLParser {
             if price == nil {
                 price = try priceElements.first()?.text()
             }
-            
             print("Price: \(price ?? "N/A")")
             
-            let colorElement = try body.select("div[data-testid='color-selector']").first()
-            let colorPElement = try colorElement?.select("p[class='d1cd7b a09145 b97b34']").first()
-            let colorName = try colorPElement?.text().replacingOccurrences(of: "-", with: "").trimmingCharacters(in: .whitespacesAndNewlines)
-            print("Color: \(colorName ?? "N/A")")
+            //MARK: - COLOR
+            var colorName: String?
             
+            // Извлечение JSON данных из скрипта
+            if let scriptElement = try body.select("script#product-schema").first() {
+                let jsonText = try scriptElement.html()
+                
+                // Парсинг JSON данных
+                if let jsonData = jsonText.data(using: .utf8) {
+                    if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                        
+                        // Извлечение цвета
+                        if let color = json["color"] as? String {
+                            colorName = color
+                            print("Color: \(color)")
+                        }
+                    }
+                }
+            }
+            
+            //MARK: - DESCRIPTION
             let descriptionElement = try body.select("#section-descriptionAccordion").first()
             let descriptions = try descriptionElement?.select("p").first()?.text()
             print("Description: \(descriptions ?? "N/A")")
             
+            //MARK: - MATERIAL
             let materialElement = try body.select("#section-materialsAndSuppliersAccordion").first()
             let material = try materialElement?.select("ul").first()?.text()
             print("Material: \(material ?? "N/A")")
             
-            let genderElement = try body.select("nav li:nth-of-type(2)").first()
-            let gender = try genderElement?.select("a").first()?.text()
+            //MARK: - GENDER
+            let genderElement = try body.select("nav[class='e0fe54']").first()
+            let genderLiElement = try genderElement?.select("li:nth-of-type(2)").first()
+            let gender = try genderLiElement?.select("a").first()?.text()
             print("Gender: \(gender ?? "N/A")")
             
-            let babyGenderElement = try body.select("hm-breadcrumbs nav ol").first()
+            //MARK: - BABY GENDER
+            let babyGenderElement = try body.select("nav[class='e0fe54'] ol").first()
             var babyGender = try babyGenderElement?.text() ?? ""
             print("BABY: \(babyGender)")
             
@@ -105,6 +123,7 @@ class HTMLParser {
                 babyGender = babyGenderCategory
             }
             
+            //MARK: - Model Product
             let product = Product()
             product.id = UUID()
             product.imageURL = imgSrc
