eabebcecde1a7ebf16c3e7aeca691e34de7b6027
KarVarr <megatr9n@gmail.com>
2024-10-25 20:50:58 +0400

    PetSnund - added new app for testing

---
+++ PetSnund/PetSnund/PetSnundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetSnundApp.swift
+//  PetSnund
+//
+//  Created by Karen Vardanian on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PetSnundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
