3b8f8736ae390109387ddfa092f2b2061aa8c44e
KarVarr <megatr9n@gmail.com>
2024-10-26 17:55:42 +0400

    PetSnund - update SubcriptionView, added cart for your subscription

--- PetSnund/PetSnund/View/Subscription/SubscriptionView.swift
+++ PetSnund/PetSnund/View/Subscription/SubscriptionView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct SubscriptionView: View {
+    
     var body: some View {
         VStack {
-            Text("Your subscription is active")
+            HStack {
+                HStack {
+                    Text("Your subscription is")
+                    Text("active")
+                        .foregroundStyle(.green)
+                }
+                Image(systemName: "dog.fill")
+            }
+            
             Text("Yerevan, Armenia. Manushian street, 100")
+                .foregroundStyle(.gray)
+            
+            Spacer()
+            
+            
+            VStack {
+                HStack {
+                    Image("food1")
+                        .resizable()
+                        .scaledToFit()
+                    VStack(alignment: .leading) {
+                        Text("Royal Canin")
+                        Text("Count: 2")
+                        Text("Item number: 1241341")
+                        Text("Warehouse: Yerevan")
+                        Text("Delivery: Every Friday")
+                    }
+                    
+                }
+                Divider()
+                Text("9 000 dram")
+                    .padding()
+                    .font(.title2)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding()
+            .shadow(radius: 10)
+            
+            Spacer()
+            
+            VStack {
+                Button("Cancel subscription") {}
+                Button("Change subscription") {}
+            }
+            .padding()
         }
+        .background(Color.gray.opacity(0.1))
     }
 }
 
