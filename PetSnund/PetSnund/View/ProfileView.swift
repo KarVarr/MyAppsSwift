1ea3f2afd0706a9ccedd41a10d819eb66719f1b7
KarVarr <megatr9n@gmail.com>
2024-10-26 15:32:16 +0400

    PetSnund - update ProfileView, added struct and list

--- PetSnund/PetSnund/View/ProfileView.swift
+++ PetSnund/PetSnund/View/ProfileView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+struct Pet: Identifiable {
+    var id: String
+    var species: String
+    var name: String
+    var age: Int
+    var gender: String
+    var breed: String
+    var allergy: String
+}
+
 struct ProfileView: View {
+    @State var pet = [
+        Pet(id: "1", species: "Cat", name: "Mia", age: 10, gender: "Female", breed: "Siamese", allergy: "None")
+    ]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("cat2")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 100, height: 100)
+        
+        List(pet) { pet in
+            Text("Name: \(pet.name)")
+            Text("Age: \(pet.age)")
+            Text("Gender: \(pet.gender)")
+            Text("Breed: \(pet.breed)")
+            Text("Allergy: \(pet.allergy)")
+        }
+        .listStyle(.plain)
     }
 }
 
