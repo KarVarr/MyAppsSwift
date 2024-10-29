a91e527da7b98cb34294d25f1adbf8858c9531f4
KarVarr <megatr9n@gmail.com>
2024-10-29 20:53:51 +0400

    PetSnund - update SettingsView, added Wallet Favorites ect

--- PetSnund/PetSnund/View/Settings/SettingsView.swift
+++ PetSnund/PetSnund/View/Settings/SettingsView.swift
@@ -9,11 +9,66 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            VStack {
+                HStack {
+                    Text("Settings")
+                        .font(.title)
+                        .padding(.leading)
+                    Spacer()
+                }
+                Image("cat1")
+                    .resizable()
+                    .frame(width:100, height:100)
+                ZStack {
+                    Rectangle()
+                        .foregroundStyle(.indigo)
+                        .frame(width: geo.size.width - 15, height: geo.size.height / 8)
+                        .clipShape( RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Wallet")
+                                .font(.title)
+                            Image(systemName: "wallet.bifold.fill")
+                        }
+                        Text("Your Balance: 0 dram")
+                            .font(.title2)
+                    }
+                    .foregroundStyle(.white)
+                }
+                ZStack {
+                    Rectangle()
+                        .foregroundStyle(.indigo)
+                        .frame(width: geo.size.width - 15, height: geo.size.height / 12)
+                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName:"heart.fill")
+                            Text("Favotites")
+                                .font(.title)
+                        }
+                    }
+                    .foregroundStyle(.white)
+                    
+                }
+                ZStack {
+                    Rectangle()
+                        .foregroundStyle(.indigo)
+                        .frame(width: geo.size.width - 15, height: geo.size.height / 12)
+                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName:"dollarsign.circle.fill")
+                            Text("Purchases")
+                                .font(.title)
+                        }
+                    }
+                    .foregroundStyle(.white)
+                }
+            }
+            .foregroundStyle(Color.black)
+            .background(Color.gray.opacity(0.1))
         }
-        .foregroundStyle(Color.black)
-        .background(Color.gray.opacity(0.1))
     }
 }
 
