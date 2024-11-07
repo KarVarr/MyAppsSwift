e546372464f6ae485e2cca6a52f5eb80efd5fc8a
KarVarr <megatr9n@gmail.com>
2024-11-07 21:07:49 +0400

    PetSnund - update ProductsView,added SwiftUIPages

--- PetSnund/PetSnund/View/Products/ProductsView.swift
+++ PetSnund/PetSnund/View/Products/ProductsView.swift
@@ -11,37 +11,9 @@ struct ProductsView: View {
     @State private var searchText = ""
     
     var body: some View {
-        NavigationStack {
-            CollectionView()
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "map.fill")
-                        }
-                    }
-                    
-                    ToolbarItem(placement: .principal) {
-                        Text("Pet Snund")
-                            .font(.title)
-                            .foregroundStyle(.primary)
-                    }
-                    
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "gear")
-                        }
-                        
-                    }
-                }
-                .navigationTitle("")
-                .navigationBarTitleDisplayMode(.inline)
-        }
-        .background(Color.gray.opacity(0.1))
-        .searchable(text: $searchText, prompt: "Look for best products")
+        CollectionView()
+            .background(Color.gray.opacity(0.1))
+            .searchable(text: $searchText, prompt: "Look for best products")
     }
 }
 
