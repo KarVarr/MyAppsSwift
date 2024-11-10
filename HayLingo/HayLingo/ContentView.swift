c3f7b73290fcdb196f4dbecff9d1d8c84f190ad0
KarVarr <megatr9n@gmail.com>
2024-11-10 14:24:29 +0400

    HayLingo - added LettersView

--- HayLingo/HayLingo/ContentView.swift
+++ HayLingo/HayLingo/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            LettersView()
+                .preferredColorScheme(.light)
+                .tabItem {
+                    Label("Letters", systemImage: "building.columns")
+                }
         }
-        .padding()
+        
     }
 }
 
---
+++ HayLingo/HayLingo/LettersView.swift
@@ -0,0 +1,18 @@
+//
+//  LettersView.swift
+//  HayLingo
+//
+//  Created by Karen Vardanian on 10.11.2024.
+//
+
+import SwiftUI
+
+struct LettersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LettersView()
+}
