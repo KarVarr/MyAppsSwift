c459d5f745cc043cbc248b88c0b3e144713c5c0b
KarVarr <megatr9n@gmail.com>
2024-11-10 14:10:16 +0400

    HayLingo - added new app

---
+++ HayLingo/HayLingo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HayLingo
+//
+//  Created by Karen Vardanian on 10.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HayLingo/HayLingo/HayLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayLingoApp.swift
+//  HayLingo
+//
+//  Created by Karen Vardanian on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HayLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
