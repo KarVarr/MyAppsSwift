70cfc71bdafeea8e87c5c0aaed08180b0600e2ca
KarVarr <megatr9n@gmail.com>
2024-11-12 20:10:19 +0400

    HayLingo - update lettersView, added Model: UserData and ProgressData

---
+++ HayLingo/Model/ UserData.swift
@@ -0,0 +1,24 @@
+//
+//   UserData.swift
+//  HayLingo
+//
+//  Created by Karen Vardanian on 12.11.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class UserData {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var selectedLanguage: String
+    var progress: [ProgressData]
+    
+    init(name: String, selectedLanguage: String, progress: [ProgressData] = []) {
+        self.id = UUID()
+        self.name = name
+        self.selectedLanguage = selectedLanguage
+        self.progress = progress
+    }
+}
---
+++ HayLingo/Model/ProgressData.swift
@@ -0,0 +1,22 @@
+//
+//  ProgressData.swift
+//  HayLingo
+//
+//  Created by Karen Vardanian on 12.11.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class ProgressData {
+    var language: String
+    var correctAnswer: Int
+    var totalQuestion: Int
+    
+    init(language: String, correctAnswer: Int, totalQuestion: Int) {
+        self.language = language
+        self.correctAnswer = correctAnswer
+        self.totalQuestion = totalQuestion
+    }
+}
