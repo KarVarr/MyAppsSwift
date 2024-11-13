2c88b253c3642c2a6b71080185bcda507912d001
KarVarr <megatr9n@gmail.com>
2024-11-13 20:20:25 +0400

    HayLingo - added LetterTrainerView

---
+++ HayLingo/HayLingo/LetterTrainerView.swift
@@ -0,0 +1,18 @@
+//
+//  LetterTrainerView.swift
+//  HayLingo
+//
+//  Created by Karen Vardanian on 13.11.2024.
+//
+
+import SwiftUI
+
+struct LetterTrainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LetterTrainerView()
+}
