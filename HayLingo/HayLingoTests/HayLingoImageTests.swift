953434f1a6d26b655f15baf10cddda1be721d8a9
KarVarr <megatr9n@gmail.com>
2024-11-20 21:32:59 +0400

    HayLingo - added unitTest

---
+++ HayLingo/HayLingoTests/HayLingoImageTests.swift
@@ -0,0 +1,13 @@
+//
+//  HayLingoImageTests.swift
+//  HayLingoTests
+//
+//  Created by Karen Vardanian on 20.11.2024.
+//
+
+import XCTest
+
+final class HayLingoImageTests: XCTestCase {
+
+    
+}
---
+++ HayLingo/HayLingoTests/HayLingoTests.swift
@@ -0,0 +1,16 @@
+//
+//  HayLingoTests.swift
+//  HayLingoTests
+//
+//  Created by Karen Vardanian on 20.11.2024.
+//
+
+import Testing
+
+struct HayLingoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
