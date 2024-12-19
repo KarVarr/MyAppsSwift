d84a475441fb13e250d272bb2d62a29a5dbedc58
KarVarr <megatr9n@gmail.com>
2024-12-19 20:41:56 +0400

    HayLingo - update, added new class SoundManager

--- HayLingo/HayLingo/Helpers/SoundManager.swift
+++ HayLingo/HayLingo/Helpers/SoundManager.swift
@@ -6,4 +6,24 @@
 //
 
 import Foundation
+import AVFoundation
 
+class SoundManager {
+    static let shared = SoundManager()
+    
+    private var soundPlayer: AVAudioPlayer?
+    
+    func playSound(name: String) {
+        guard let soundURL = Bundle.main.url(forResource: name, withExtension: "mp3") else {
+            print("Sound CLICK file not found.")
+            return
+        }
+        
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            soundPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+}
